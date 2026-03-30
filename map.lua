be95ff742a7133a3fa7c8e17886602c1b08c11e3
Prithic P <prithicprithic2000@gmail.com>
2026-03-30 13:16:51 +0530

    Implemented Map:draw() and loaded tree texture in map.lua

--- map.lua
+++ map.lua
@@ -2,19 +2,50 @@ local Map= {}
 Map.__index = Map
 
 function Map.new()
-    local self = setmetatable({}, Map)
+  local self = setmetatable({}, Map)
 
-    self.titles = {
-        {1,1,1,1,1,1,1,1,1,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,1,1,0,0,0,1},
-        {1,0,0,0,0,0,0,1,0,1},
-        {1,0,1,1,0,0,0,1,0,1},
-        {1,0,0,1,0,0,0,0,0,1},
-        {1,0,0,0,0,1,1,0,0,1},
-        {1,0,0,0,0,0,0,0,0,1},
-        {1,1,1,1,1,1,1,1,1,1},
-    }
+  self.tiles = {
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,1,1,0,0,0,1},
+    {1,0,0,0,0,0,0,1,0,1},
+    {1,0,1,1,0,0,0,1,0,1},
+    {1,0,0,1,0,0,0,0,0,1},
+    {1,0,0,0,0,1,1,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1},
+  }
+
+  self.tileSize = 32
+  self.treeImage = love.graphics.newImage("Resources/tree.png")
+  self.treeScaleX = self.tileSize / self.treeImage:getWidth()
+  self.treeScaleY = self.tileSize / self.treeImage:getHeight()
+
+  return self
+end
+
+function Map:draw()
+  for y = 1, #self.tiles do
+    for x = 1, #self.tiles[y] do
+      local tile = self.tiles[y][x]
+      local drawX = (x - 1) * self.tileSize
+      local drawY = (y - 1) * self.tileSize
+
+      if tile == 1 then
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.draw(
+          self.treeImage,
+          drawX,
+          drawY,
+          0,
+          self.treeScaleX,
+          self.treeScaleY
+        )
+      end
+    end
+  end
+
+  love.graphics.setColor(1,1,1)
 end
 
 return Map
