7218724d7a62cf8a63b4233130c049cb6f889341
Prithic P <prithicprithic2000@gmail.com>
2026-03-30 13:18:13 +0530

    Added basic collision detection (Map:collides) to map.lua

--- map.lua
+++ map.lua
@@ -48,4 +48,26 @@ function Map:draw()
   love.graphics.setColor(1,1,1)
 end
 
+function Map:collides(px, py, size)
+  local half = size / 2
+  local left = px - half
+  local right = px + half
+  local top = py - half
+  local bottom = py + half
+
+  local tileSize = self.tileSize
+
+  local function tileAt(px, py)
+    local tx = math.floor(px / tileSize) + 1
+    local ty = math.floor(py / tileSize) + 1
+    return self.tiles[ty] and self.tiles[ty][tx]
+  end
+
+  return
+    tileAt(left, top) == 1 or
+    tileAt(right, top) == 1 or
+    tileAt(left, bottom) == 1 or
+    tileAt(right, bottom) == 1
+end
+
 return Map
